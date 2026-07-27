5c124be942ebcf79ef558d594b7ae4e23d065179
terryfejiro5-tech <terryfejiro5@gmail.com>
2026-07-27 11:57:45 +0100

    Create products.js

---
+++ products.js
@@ -0,0 +1,12 @@
+const products = [
+  {
+    name: "Luxury Coach Shiny Slide",
+    image: "https://i.postimg.cc/Hkrzc9DN/IMG-4453.jpg",
+    category: "slide",
+    oldPrice: "₦15,000",
+    price: "₦13,000",
+    colours: ["Black"],
+    sizes: ["37", "38", "39", "40", "41", "42"],
+    rating: "5.0"
+  }
+];
