e7b37e1e108980484bc72d6538a75ebacdb3a4d5
terryfejiro5-tech <terryfejiro5@gmail.com>
2026-07-27 23:27:04 +0100

    Update products.js

--- products.js
+++ products.js
@@ -8,5 +8,16 @@ const products = [
     colours: ["Black"],
     sizes: ["37", "38", "39", "40", "41", "42"],
     rating: "5.0"
+  },
+
+  {
+    name: "Luxury Ladies Bag",
+    image: "https://i.postimg.cc/g0vwR10y/4fd4c454-49c1-402a-a076-85cc4b7d6c61.jpg",
+    category: "bag",
+    oldPrice: "₦25,000",
+    price: "₦21,000",
+    colours: [],
+    sizes: [],
+    rating: "4.9"
   }
 ];
